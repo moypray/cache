124988af5bc98783a3e90729a84f64f38bbace63
Hu Keping <hukeping@huawei.com>
2015-05-16 22:51:35 +0800

    Rename package name
    Buffer to Cache.
    
    Signed-off-by: Hu Keping <hukeping@huawei.com>

--- llrb-stats.go
+++ llrb-stats.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package buffer
+package cache
 
 // Len returns the number of nodes in the tree.
 func (t *LLRB) Len() int { return t.count }
